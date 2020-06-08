c80adb5227c5e8dbcd0bbef8ddab115b68e0aa86
John <johndoemisc@tutamail.com>
2020-06-08 16:40:12 +0300

    Add getSecretWord action creator on App mount

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import App from './App';
+import App, { UnconnectedApp } from './App';
 import { findByTestAttr, storeFactory } from '../test/testUtils';
 
 const setup = (initialState={}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<App store={store} />).dive();
-  console.log(wrapper.debug());
+  const wrapper = shallow(<App store={store} />).dive().dive();
   return wrapper;
 }
 
@@ -35,4 +34,23 @@ describe('Redux props', () => {
     const getSecretWordProp = wrapper.instance().props.getSecretWord;
     expect(getSecretWordProp).toBeInstanceOf(Function);
   })
+})
+
+test('`getSecretWord` runs on App mount', () => {
+  const getSecretWordMock = jest.fn();
+  const props = {
+    getSecretWord: getSecretWordMock,
+    success: false,
+    guessedWords: [],
+  }
+
+  // set up app component with getSecretWordMock as the getSecretWord prop
+  const wrapper = shallow(<UnconnectedApp {...props} />);
+
+  // run lifecycle method
+  wrapper.instance().componentDidMount();
+
+  // check to see if mock ran
+  const getSecretWordCallCount = getSecretWordMock.mock.calls.length;
+  expect(getSecretWordCallCount).toBe(1);
 })
